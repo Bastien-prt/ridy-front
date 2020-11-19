3d308e4355056ddac998b7cbe1663450eebc628f
Bastien Perrot <bastien.perrot1@gmail.com>
2020-11-19 13:45:30 +0100

    premiere route HomePage

---
+++ ridy-front/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import HomePage from './components/home/HomePage'
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+
+        <Route exact path="/" component={HomePage}/>
+
+      </Switch>
+     
+    </div>
+  );
+}
+
+export default App;

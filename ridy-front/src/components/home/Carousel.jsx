25486c3ae26ded03630b7797604584b3ab11110d
nasrat <nasrat.noury@gmail.com>
2020-11-19 17:59:50 +0100

    emptyslide

--- ridy-front/src/components/home/Carousel.jsx
+++ ridy-front/src/components/home/Carousel.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import "./Carousel.css";
+import { Carousel } from "react-responsive-carousel";
 
-
-const Carousel = () => {
-    return (
-        <div> Hello</div>
-    )
-}
-
-export default Carousel;
+export default () => (
+  <div className="carousel">
+    <Carousel autoPlay>
+      <div>
+        <img src="https://media.senscritique.com/media/000010056037/source_big/Halo_Combat_Evolved.jpg" />
+        <p className="legend1">Legend 1</p>
+      </div>
+      <div>
+        <img src="https://images-na.ssl-images-amazon.com/images/I/51rgXbmpRGL._AC_SY445_.jpg" />
+        <p className="legend2">Legend 2</p>
+      </div>
+      <div>
+        <img src="https://store-images.s-microsoft.com/image/apps.8340.69990143727992974.1a013d7b-2e62-4f4d-a174-a07a940b8151.a7e03e01-7cc2-4818-b25f-fb14bdad643c?mode=scale&q=90&h=225&w=150" />
+        <p className="legend3">Legend 3</p>
+      </div>
+    </Carousel>
+  </div>
+);
--- ridy-front/src/components/home/Home.jsx
+++ ridy-front/src/components/home/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import { render } from "react-dom";
+import Carousel from "./Carousel.jsx";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Home = () => {
-    return (
-        <div> Page home </div>
-    )
-}
+  return (
+    <div>
+      {" "}
+      Page home
+      <Carousel />
+    </div>
+  );
+};
+
+// render(<App />, document.getElementById("root"));
 
 export default Home;

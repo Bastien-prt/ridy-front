407fe6409468d01e543f187e879f3338a5910821
Stella <stella.dmt@gmail.com>
2020-11-20 03:38:41 +0100

    filtered cities

--- ridy-front/src/components/home/Header.jsx
+++ ridy-front/src/components/home/Header.jsx
@@ -1,40 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import '../../style/css/Header.css'
+import "../../style/css/Header.css";
 
 const Header = () => {
-    return (
-        <div>
-            <div className="container-content-banner">
-                <div className="container-text-presentation">
-                    <h1 className="presentation"> Rent your ride <p> anytime</p> <p>anywhere  </p> </h1>
-                    <p className="informations"> We will provide you some carrots ... </p>
-                </div>
-                
-            </div>
-            <div className="container-where">
-                <p className="rent"> Where do you want to rent ? </p>
+	const [city, setCity] = useState("");
 
-                <select name="city" id="city">
-                    <option value="">-- Please choose a city --</option>
-                    <option value="bordeaux"> Bordeaux </option>
-                    <option value="nantes"> Nantes </option>
-                    <option value="toulouse"> Toulouse </option>
-                    <option value="brest"> Brest </option>
-                    <option value="bayonne"> Bayonne </option>
-                    <option value="lyon"> Lyon </option>
-                </select>
+	const HandleCity = (e) => {
+		setCity(e.target.value);
+	};
+	return (
+		<div>
+			<div className="container-content-banner">
+				<div className="container-text-presentation">
+					<h1 className="presentation">
+						{" "}
+						Rent your ride <p> anytime</p> <p>anywhere </p>{" "}
+					</h1>
+					<p className="informations"> We will provide you some carrots ... </p>
+				</div>
+			</div>
+			<div className="container-where">
+				<p className="rent"> Where do you want to rent ? </p>
 
-                {/* <input className="input-city" type="text" name="" id="" placeholder="Please, enter your city"/> */}
-            </div>
+				<select name="city" id="city" onChange={HandleCity}>
+					<option value="">-- Please choose a city --</option>
+					<option value="Bordeaux"> Bordeaux </option>
+					<option value="Nantes"> Nantes </option>
+					<option value="Toulouse"> Toulouse </option>
+					<option value="Brest"> Brest </option>
+					<option value="Bayonne"> Bayonne </option>
+					<option value="Lyon"> Lyon </option>
+				</select>
+			</div>
 
-            <div className="container-boutton">
-                <Link to="/Map" className="link-search"  >  Search </Link>
-            </div>
-            
-
-        </div>
-    )
-}
+			<div className="container-boutton">
+				<Link
+					to={{
+						pathname: "/Map",
+						state: { city },
+					}}
+					className="link-search"
+				>
+					{" "}
+					Search{" "}
+				</Link>
+			</div>
+		</div>
+	);
+};
 
 export default Header;
--- ridy-front/src/components/map/MapContainer.jsx
+++ ridy-front/src/components/map/MapContainer.jsx
@@ -1,88 +1,107 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
 import "../../style/css/MapContainer.css";
-import axios from "axios";
 
 const mapStyles = {
-  width: "100%",
-  height: "100%",
+	width: "100%",
+	height: "100%",
 };
 
-const MapContainer = ({ google }) => {
-  const [info, setInfo] = useState("");
-  const [newArray, setNewArray] = useState([]);
-  const [objectStore, setObjectStore] = useState({});
+const MapContainer = ({ google, location }) => {
+	const { city } = location.state;
+	const [info, setInfo] = useState("");
+	const [newArray, setNewArray] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(`https://gitbusters.herokuapp.com/api/rides/`)
-      .then((response) => response.data)
-      .then((data) => setNewArray(data));
-  }, []);
+	useEffect(() => {
+		axios
+			.get("https://gitbusters.herokuapp.com/api/rides")
+			.then((result) => result.data)
+			.then((data) => setNewArray(data.filter((item) => item.city === city)));
+	}, [city]);
 
-  const [stores, setStores] = useState([
-    { latitude: 44.8115510558411, longitude: -0.5972854563965946 },
-    { latitude: 44.80475549452777, longitude: -0.6309465089812427 },
-    { latitude: 44.84089121384058, longitude: -0.5808519179932325 },
-    { latitude: 44.83502127197189, longitude: -0.574980164319232 },
-    { latitude: 44.849623961794585, longitude: -0.5751784382387548 },
-    { latitude: 44.85589543401733, longitude: -0.5903870243578702 },
-    { latitude: 44.84873951631123, longitude: -0.5588456712662726 },
-    { latitude: 44.83020363562137, longitude: -0.5272349788806885 },
-    { latitude: 44.8443910864143, longitude: -0.6520393168553262 },
-    { latitude: 44.88140146099171, longitude: -0.6122024361043721 },
-  ]);
+	let coordCity = {};
+	let coordBordeaux = { lat: 44.833, lng: -0.5667 };
+	let coordNantes = { lat: 47.218371, lng: -1.553621 };
+	let coordToulouse = { lat: 43.604652, lng: 1.444209 };
+	let coordBrest = { lat: 48.390394, lng: -4.486076 };
+	let coordBayonne = { lat: -1.474841, lng: 43.492949 };
+	let coordLyon = { lat: 45.764043, lng: 4.835659 };
 
-  const handleClick = (array) => {
-    setInfo(
-      <div className='boxPoney'>
-        <img className='image-map' src={array.photo} alt='yo' />
-        <div className='box-info-poney'>
-          <div className='info'> {array.name} </div>
-          <div className='info'>
-            {" "}
-            {array.seat} people - {array.price} golden coins{" "}
-          </div>
-        </div>
-        <Link to={`/description/${array.id}`}>
-          <button className='bookNow'>Book now</button>
-        </Link>
-      </div>
-    );
-  };
+	switch (city) {
+		default:
+			coordCity = coordBordeaux;
+			break;
+		case "Nantes":
+			coordCity = coordNantes;
+			break;
+		case "Toulouse":
+			coordCity = coordToulouse;
+			break;
+		case "Brest":
+			coordCity = coordBrest;
+			break;
+		case "Bayonne":
+			coordCity = coordBayonne;
+			break;
+		case "Lyon":
+			coordCity = coordLyon;
+			break;
+	}
 
-  const displayMarkers = (e) => {
-    return newArray.map((array) => {
-      return (
-        <Marker
-          key={array.id}
-          id={array.id}
-          position={{
-            lat: array.latitude,
-            lng: array.longitude,
-          }}
-          onClick={() => handleClick(array)}
-        />
-      );
-    });
-  };
+	const handleClick = (array) => {
+		setInfo(
+			<div className="boxPoney">
+				<img className="image-map" src={array.photo} alt="yo" />
+				<div className="box-info-poney">
+					<div className="info"> {array.name} </div>
+					<div className="info">
+						{" "}
+						{array.seat} {array.seat > 1 ? "people" : "person"} - {array.price}{" "}
+						golden coin{array.price > 1 && "s"}
+					</div>
+				</div>
+				<Link to={`/description/${array.id}`}>
+					<button className="bookNow">More info</button>
+				</Link>
+			</div>
+		);
+	};
 
-  return (
-    <div className='map-box'>
-      <p className='test1'>{info}</p>
-      <Map
-        google={google}
-        zoom={11}
-        style={mapStyles}
-        initialCenter={{ lat: 44.833, lng: -0.5667 }}>
-        {displayMarkers()}
-      </Map>
-    </div>
-  );
+	const displayMarkers = (e) => {
+		return newArray.map((array) => {
+			return (
+				<Marker
+					key={array.id}
+					id={array.id}
+					position={{
+						lat: array.latitude,
+						lng: array.longitude,
+					}}
+					onClick={() => handleClick(array)}
+				/>
+			);
+		});
+	};
+
+	return (
+		<div className="map-box">
+			<p className="test1">{info}</p>
+
+			<Map
+				google={google}
+				zoom={11}
+				style={mapStyles}
+				initialCenter={coordCity}
+			>
+				{displayMarkers()}
+			</Map>
+		</div>
+	);
 };
 
 export default GoogleApiWrapper({
-  apiKey: "AIzaSyDQ3MLXcFAxS3N8ZCdSYloha8UsTNk8h3Q",
+	apiKey: "AIzaSyDQ3MLXcFAxS3N8ZCdSYloha8UsTNk8h3Q",
 })(MapContainer);

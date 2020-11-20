90b5dcb13755662e92799444882259235f361172
RomainMouly <romainmouly@gmail.com>
2020-11-20 01:44:03 +0100

    axios headertomap

--- ridy-front/src/components/map/Map.jsx
+++ ridy-front/src/components/map/Map.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
+const Map = ({ location }) => {
+  const { city } = location.state;
+  const [cityRides, setCityRides] = useState([]);
 
-const Map = () => {
-    return (
-        <div> Hello sur map</div>
-    )
-}
+  useEffect(() => {
+    axios
+      .get(`https://gitbusters.herokuapp.com/api/rides`)
+      .then((result) => result.data)
+      .then((data) =>
+        setCityRides(data.filter((cityRide) => cityRide.city === city))
+      );
+  }, [city]);
+
+  return (
+    <div>
+      {cityRides.map((cityRideOne) => (
+        <div> {cityRideOne.name}</div>
+      ))}
+    </div>
+  );
+};
 
 export default Map;
